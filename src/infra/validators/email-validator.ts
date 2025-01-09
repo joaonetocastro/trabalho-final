8c0182d87d0aec24c0017bb870a881817272f956
João Neto C <j.neto111@gmail.com>
2025-01-09 16:23:39 -0300

    feat: should return false if @ is not present

--- src/infra/validators/email-validator.ts
+++ src/infra/validators/email-validator.ts
@@ -2,6 +2,7 @@ import { Validator } from "@/presentation/validator";
 
 export class EmailValidator implements Validator {
   validate(email: any): boolean {
+    if (!email.includes('@')) return false
     return true
   }
 }
--- test/infra/validators/email-validator.test.ts
+++ test/infra/validators/email-validator.test.ts
@@ -4,3 +4,7 @@ it('should return true if email is valid', () => {
   const validator = new EmailValidator()
   expect(validator.validate('example@mail.com')).toBe(true)
 })
+it('should return false if @ is not present', () => {
+  const validator = new EmailValidator()
+  expect(validator.validate('examplemail.com')).toBe(false)
+})
