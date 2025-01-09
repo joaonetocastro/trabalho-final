85d4638e3d3e3cdea4fb507a6acc3141f0175e5c
João Neto C <j.neto111@gmail.com>
2025-01-09 16:25:01 -0300

    feat: should return false if value received is not a string

--- test/infra/validators/email-validator.test.ts
+++ test/infra/validators/email-validator.test.ts
@@ -4,7 +4,13 @@ it('should return true if email is valid', () => {
   const validator = new EmailValidator()
   expect(validator.validate('example@mail.com')).toBe(true)
 })
+
 it('should return false if @ is not present', () => {
   const validator = new EmailValidator()
   expect(validator.validate('examplemail.com')).toBe(false)
+})
+
+it('should return false if value received is not a string', () => {
+  const validator = new EmailValidator()
+  expect(validator.validate(123)).toBe(false)
 })
