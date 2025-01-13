ad136a0d9daca25b932cc02af036fae4a2362cb5
Edilson Morais <moraisbritoedilson@gmail.com>
2025-01-13 15:33:00 +0900

    fix: change tests to DRIVER

--- test/application/use-cases/create-account-use-case.test.ts
+++ test/application/use-cases/create-account-use-case.test.ts
@@ -1,28 +1,28 @@
-import { CreateAccountUseCase } from "../../../src/application/use-cases/create-account-use-case";
+import { CreateAccountUseCase } from '../../../src/application/use-cases/create-account-use-case'
 
-describe("CreateAccountUseCase", () => {
-  let createAccountUseCase: CreateAccountUseCase;
+describe('CreateAccountUseCase', () => {
+  let createAccountUseCase: CreateAccountUseCase
 
   beforeEach(() => {
-    createAccountUseCase = new CreateAccountUseCase();
-  });
+    createAccountUseCase = new CreateAccountUseCase()
+  })
 
-  it("should create a valid user account", async () => {
-    expect(await createAccountUseCase.execute({} as any)).toBe(true);
-  });
-  it("should create a valid motorist account", async () => {
-    expect(await createAccountUseCase.execute({} as any)).toBe(true);
-  });
-  it("should display an error message if the account already exists", async () => {
-    expect(await createAccountUseCase.execute({} as any)).toBe(true);
-  });
-  it("should display error message if the account is not created", async () => {
-    expect(await createAccountUseCase.execute({} as any)).toBe(true);
-  });
-  it("should not create an account with invalid email", async () => {
-    expect(await createAccountUseCase.execute({} as any)).toBe(true);
-  });
-  it("should not create an account with invalid cpf", async () => {
-    expect(await createAccountUseCase.execute({} as any)).toBe(true);
-  });
-});
+  it('should create a valid user account', async () => {
+    expect(await createAccountUseCase.execute({} as any)).toBe(true)
+  })
+  it('should create a valid driver account', async () => {
+    expect(await createAccountUseCase.execute({} as any)).toBe(true)
+  })
+  it('should display an error message if the account already exists', async () => {
+    expect(await createAccountUseCase.execute({} as any)).toBe(true)
+  })
+  it('should display error message if the account is not created', async () => {
+    expect(await createAccountUseCase.execute({} as any)).toBe(true)
+  })
+  it('should not create an account with invalid email', async () => {
+    expect(await createAccountUseCase.execute({} as any)).toBe(true)
+  })
+  it('should not create an account with invalid cpf', async () => {
+    expect(await createAccountUseCase.execute({} as any)).toBe(true)
+  })
+})
