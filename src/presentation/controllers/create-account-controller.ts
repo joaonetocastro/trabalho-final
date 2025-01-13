33370f1d974810e06d1c025439e17898c4c5c8e8
João Neto C <j.neto111@gmail.com>
2025-01-13 20:14:08 -0300

    feat: should throw error if validation fails

--- src/presentation/controllers/create-account-controller.ts
+++ src/presentation/controllers/create-account-controller.ts
@@ -1,10 +1,17 @@
 import { CreateAccount } from "@/domain/entities";
 import { Controller } from "../controller";
+import { Validator } from "../validator";
 
 export class CreateAccountController implements Controller {
-  constructor(readonly createAccountUseCase: CreateAccount) { }
+  constructor(
+    readonly createAccountUseCase: CreateAccount,
+    readonly bodyValidator: Validator
+  ) { }
 
   async execute(body: any): Promise<any> {
+    if (!this.bodyValidator.validate(body)) {
+      throw new Error('Failed to validate body.')
+    }
     return await this.createAccountUseCase.execute(body)
   }
 }
--- test/presentation/controllers/create-account-controller.test.ts
+++ test/presentation/controllers/create-account-controller.test.ts
@@ -1,11 +1,16 @@
 import { AccountType } from "@/domain/@types/account";
 import { CreateAccount, User } from "@/domain/entities";
 import { CreateAccountController } from "@/presentation/controllers/create-account-controller"
+import { Validator } from "@/presentation/validator";
 import { mock } from 'jest-mock-extended';
 
 it('should return an account if everything is ok', async () => {
   const stub = mock<CreateAccount>()
-  const controller = new CreateAccountController(stub)
+
+  const validator = mock<Validator>()
+  validator.validate.mockReturnValue(true)
+
+  const controller = new CreateAccountController(stub, validator)
   const body = {
     id: '123',
     name: 'my name',
@@ -21,4 +26,27 @@ it('should return an account if everything is ok', async () => {
   const response = await controller.execute(body)
 
   expect(response.id).toEqual(body.id)
+})
+
+it('should throw error if validation fails', async () => {
+  const stub = mock<CreateAccount>()
+
+  const validator = mock<Validator>()
+  validator.validate.mockReturnValue(false)
+
+  const controller = new CreateAccountController(stub, validator)
+  const body = {
+    id: '123',
+    name: 'my name',
+    email: 'example@example.com',
+    cpf: '11111111111',
+    password: 'any_password',
+    car_plate: 'OCXD1212',
+    type: AccountType.DRIVER,
+    password_algorithm: 'bcrypt'
+  }
+
+  stub.execute.mockResolvedValueOnce(new User(body))
+
+  expect(controller.execute(body)).rejects.toThrow(new Error('Failed to validate body.'))
 })
