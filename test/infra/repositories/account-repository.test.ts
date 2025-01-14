fa675c31897552e2766c0e6f0da14845b225ac5c
Daniel-Farias <danielmfariasdev@gmail.com>
2025-01-14 15:26:10 -0300

    feat: test to verify that should not create account if email existsy

--- test/infra/repositories/account-repository.test.ts
+++ test/infra/repositories/account-repository.test.ts
@@ -20,4 +20,22 @@ describe("Account Repository", () => {
     expect(accountCreated.email).toBe(account.email);
     expect(accountCreated.cpf).toBe(account.cpf);
   });
+
+  test("should not create an account if email already exists", async () => {
+    const account = {
+      name: "test_02",
+      email: "test_02@email.com",
+      password: "test_02",
+      id: "1",
+      cpf: "11221212121",
+      type: AccountType.DRIVER,
+      password_algorithm: "bcrypt",
+    };
+
+    const repository = new AccountRepository();
+    await repository.createAccount(account);
+    expect(() => repository.createAccount(account)).rejects.toThrow(
+      new Error("Email already exists")
+    );
+  });
 });
